db2a674b71b65e52609f82991d85fa6cf9e99994
websolucionOrg <131605739+websolucionOrg@users.noreply.github.com>
2023-04-25 17:10:22 +0000

    kolk

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,10 @@
 import { HeadHtml } from '@/src/components/elements/HeadH'
-import Link from 'next/link'
-import Image from 'next/image'
+import { HomePage } from '@/src/components/Home/Home';
 export default function Home({data}) {
   return (
     <>
       <HeadHtml titlePage="Home-Welcome" />
-       <section className='starthomePage'>
-          <div className='content'>
-            <h1>O melhor para os nossos Pets</h1>
-            <p> O melhor laboratorio de medicina molecular do mundo </p>
-            <Link href="/" passHref className='btn' >Saber Mais </Link>
-          </div>
-       </section>
-
-       <section className='animaisTypes'>
-          <div className='container'>
-            {data.map((ev)=>(
-              <Link href={`/${ev.pet}`} passHref className='box' >
-                    <Image src={ev.img} width={500} alt='Sambo' height={400} />
-                    <h2> {ev.title} </h2>
-              </Link>                
-              ))}
-          </div>
-       </section>
+      <HomePage data={data} />
     </>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,1 +1,28 @@
-rafc
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export const HomePage = ({data}) => {
+  return (
+    <>   
+       <section className='starthomePage'>
+          <div className='content'>
+            <h1>O melhor para os nossos Pets</h1>
+            <p> O melhor laboratorio de medicina molecular do mundo </p>
+            <Link href="/" passHref className='btn' >Saber Mais </Link>
+          </div>
+       </section>
+
+       <section className='animaisTypes'>
+          <div className='container'>
+            {data.map((ev)=>(
+              <Link href={`/${ev.pet}`} passHref className='box' >
+                    <Image src={ev.img} width={500} alt='Sambo' height={400} />
+                    <h2> {ev.title} </h2>
+              </Link>                
+              ))}
+          </div>
+       </section>
+    </>
+  )
+}
