f017621a3f66a9ff60ca5f5de5051cdb14d55afd
websolucionOrg <131605739+websolucionOrg@users.noreply.github.com>
2023-04-26 21:07:42 +0000

    AAD

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
-import { FaGithub, FaFacebookF } from "react-icons/fa";
+import { FaGithub, FaFacebookF,FaGitlab, FaPinterest } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className='footer'>
-        <p> &copy; 2023 Websolucion </p>
+        <p>Websolucion &copy; 2023  </p>
         <div className='icons' >
-          <Link href="https://github.com"> <FaGithub /> </Link>
+          <Link href="https://github.com" > <FaGithub /> </Link>
           <Link href="https://github.com"> <FaFacebookF /> </Link>
+          <Link href="https://github.com"> <FaGitlab /> </Link>
+          <Link href="https://github.com"> <FaPinterest /> </Link>
+
         </div>
     </footer>
   )
