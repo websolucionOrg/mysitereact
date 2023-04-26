4de0c57d498bc56f3d5356b1e29f0ce3d38ed649
websolucionOrg <131605739+websolucionOrg@users.noreply.github.com>
2023-04-26 05:42:05 +0000

    ettte

--- pages/animals/[cat]/index.js
+++ pages/animals/[cat]/index.js
@@ -1,14 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
+import { HeadHtml } from '@/src/components/elements/HeadH'
 
-const index = () => {
+const TypeAnimals = ({data}) => {
   return (
-    <div>index</div>
+   
+    <>
+    <HeadHtml titlePage="Animais 1" />
+    {data.map((ev)=>(
+    <Link href={`/${ev.pet}`} > {ev.pet} </Link>
+    ))}
+    
+    </>
   )
 }
 
-export default index
+export default TypeAnimals
 
-export async function getStaticPaths(){
-    const {pats} = import("/data/data.json")
-    console.log(pats)
+export async function getStaticPaths() {
+  const { pets } = await import('/data/data.json');
+  console.log(pets);
+  const allPets = pets.map((ev) => {
+    return {
+      params: {
+        cat: ev.pet.toString(),
+      },
+    };
+  });
+  console.log(allPets);
+  return {
+    paths: allPets,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const id = context?.params.cat;
+  const { allPets} = await import('/data/data.json');
+
+  const data = allPets.filter((ev) => ev.pet === id);
+
+  return { props: { data, pageName: id } };
 }
