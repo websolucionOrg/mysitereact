d23e1cadfed60aff83ff07fdd1cee39be7b82603
websolucionOrg <131605739+websolucionOrg@users.noreply.github.com>
2023-04-26 06:51:26 +0000

    ddfdf

--- pages/animals/[cat]/index.js
+++ pages/animals/[cat]/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
 import { HeadHtml } from '@/src/components/elements/HeadH'
-
+import Image from 'next/image'
 const TypeAnimals = ({data}) => {
   return (
    
-    <>
+    <div>
     <HeadHtml titlePage="Animais 1" />
     {data.map((ev)=>(
-    <Link href={`/${ev.pet}`} > {ev.pet} </Link>
+    <Link href={`/${ev.pet}`} > 
+      <h2>{ev.desc}</h2>
+        <Image src={ev.image} alt={ev.id} width={400} height={400} />
+    </Link>
     ))}
     
-    </>
+    </div>
   )
 }
 
@@ -20,26 +23,26 @@ export default TypeAnimals
 export async function getStaticPaths() {
   const { pets } = await import('/data/data.json');
   console.log(pets);
-  const allPets = pets.map((ev) => {
+  const allPaths = pets.map((ev) => {
     return {
       params: {
         cat: ev.pet.toString(),
       },
     };
   });
-  console.log(allPets);
+  console.log(allPaths);
   return {
-    paths: allPets,
+    paths: allPaths,
     fallback: false,
   };
 }
 
 export async function getStaticProps(context) {
-  console.log(context);
+  
   const id = context?.params.cat;
   const { allPets} = await import('/data/data.json');
 
-  const data = allPets.filter((ev) => ev.pet === id);
+  const data = allPets.filter((ev) => ev.tyPet === id);
 
   return { props: { data, pageName: id } };
 }
